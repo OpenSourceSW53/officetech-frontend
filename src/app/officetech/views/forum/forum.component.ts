98de8dbc68d2262741fbb68a93a93b80ab744baf
Arian Rodriguez <arianmrv12@gmail.com>
2024-04-25 21:22:53 -0500

    feature: changing view depending type user

--- src/app/officetech/views/forum/forum.component.ts
+++ src/app/officetech/views/forum/forum.component.ts
@@ -2,19 +2,21 @@ import {Component, OnInit} from '@angular/core';
 import {CommentComponent} from "../../components/comment/comment.component";
 import {ForumService} from "../../services/forum.service";
 import ForumCommentEntity from "../../entities/forum-comment.entity";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-forum',
   standalone: true,
   imports: [
     CommentComponent,
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './forum.component.html',
   styleUrl: './forum.component.css'
 })
 export class ForumComponent implements OnInit {
+  type_user: number = 0; // here will be the type of the user bringing by event input, 1: team, 0: technician
   data: any[] = [];
   answers: any[] = [];
 
@@ -38,6 +40,10 @@ export class ForumComponent implements OnInit {
 
   receiveAnswers(answers: any[]) {
     this.answers = answers;
+  }
+
+  setTypeUser(type: number) {
+    this.type_user = type;
   }
 
 }
