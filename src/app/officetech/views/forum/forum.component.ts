cc3b5e634c8946a90c1c0ce5227eb17599930d14
Arian Rodriguez <arianmrv12@gmail.com>
2024-04-25 20:12:08 -0500

    add: forum get posts

---
+++ src/app/officetech/views/forum/forum.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {CommentComponent} from "../../components/comment/comment.component";
+import {ForumService} from "../../services/forum.service";
+import ForumCommentEntity from "../../entities/forum-comment.entity";
+import {NgForOf} from "@angular/common";
+
+@Component({
+  selector: 'app-forum',
+  standalone: true,
+  imports: [
+    CommentComponent,
+    NgForOf
+  ],
+  templateUrl: './forum.component.html',
+  styleUrl: './forum.component.css'
+})
+export class ForumComponent implements OnInit {
+  data: any[] = [];
+  constructor(private forumService: ForumService){
+
+  }
+
+  async ngOnInit() {
+    await this.getForumPosts();
+  }
+
+  async getForumPosts() {
+    const result = await this.forumService.getForumPosts();
+
+    for(let e in result) {
+      const post = new ForumCommentEntity(result[e].image, result[e].name, result[e].title, result[e].description);
+
+      this.data.push(post);
+    }
+  }
+
+}
