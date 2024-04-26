3b0236c8d52bdf0c0a93d1b6f92a2e2a323d5a46
Arian Rodriguez <arianmrv12@gmail.com>
2024-04-25 20:10:49 -0500

    feature: forum to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "../public/header/header.component";
+import {ForumComponent} from "./officetech/views/forum/forum.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ForumComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
