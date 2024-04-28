4a4dde4fe9ca305d21efc86c0896d27aa653e191
QuiqueVladimir <U202022365@upc.edu.pe>
2024-04-28 18:24:49 -0500

    feat(edit-profile): add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {ProfileComponent} from "./officetech/components/profile/profile.component";
+import {EditProfileComponent} from "./officetech/components/edit-profile/edit-profile.component";
 
-export const routes: Routes = [
+
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'edit-profile', component: EditProfileComponent },
+  { path: '', redirectTo: '/profile', pathMatch: 'full' }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
