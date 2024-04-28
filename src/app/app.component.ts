9117268d74e29656f69f1e86b5a24838c694b5c6
QuiqueVladimir <U202022365@upc.edu.pe>
2024-04-28 17:50:08 -0500

    feat(profile-company): added user profile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "../public/header/header.component";
+import {ProfileComponent} from "./officetech/components/profile/profile.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
