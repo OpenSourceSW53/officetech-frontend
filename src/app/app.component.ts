ce0a8dc671970e42c1a73d385a2d950610501f4f
QuiqueVladimir <U202022365@upc.edu.pe>
2024-04-28 18:07:12 -0500

    feat(edit-profile): added user data editing view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "../public/header/header.component";
 import {ProfileComponent} from "./officetech/components/profile/profile.component";
+import {EditProfileComponent} from "./officetech/components/edit-profile/edit-profile.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ProfileComponent],
+  imports: [RouterOutlet, HeaderComponent, ProfileComponent, EditProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
