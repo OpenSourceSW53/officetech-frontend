092eac43710444a236e6d73c9f13002b6e311b5b
QuiqueVladimir <U202022365@upc.edu.pe>
2024-04-28 18:07:42 -0500

    feat(edit-profile): added user data editing view

---
+++ src/app/officetech/components/profile/profile.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent implements OnInit {
+  name: string = "";
+  email: string = "";
+  phone: string = "";
+  imgUrl: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Aquí podrías cargar los datos del usuario desde algún servicio o API
+    this.name = "Nombre del Usuario";
+    this.email = "usuario@example.com";
+    this.phone = "123456789";
+    this.imgUrl = "url_de_la_imagen";
+  }
+}
