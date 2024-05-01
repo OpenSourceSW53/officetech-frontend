9c1fe1d43eef0cd95f93976fcd01b442fb03785a
Arian Rodriguez <arianmrv12@gmail.com>
2024-05-01 13:39:16 -0500

    update: creating new form

--- src/app/public/pages/forum/forum.component.ts
+++ src/app/public/pages/forum/forum.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import { CommentComponent } from '../../../officetech/components/comment/comment.component';
 import { ResponsesComponent } from '../../../officetech/components/responses/responses.component';
 import ForumCommentEntity from "../../../officetech/models/forum-comment.entity";
@@ -12,6 +12,8 @@ import {
   MatDialogContent,
   MatDialogTitle
 } from "@angular/material/dialog";
+import {FormsModule} from "@angular/forms";
+import {AuthService} from "../../../shared/services/auth/auth.service";
 
 @Component({
   selector: 'app-forum',
@@ -24,38 +26,65 @@ import {
     MatDialogContainer,
     MatDialogTitle,
     MatDialogContent,
-    MatDialogActions
+    MatDialogActions,
+    FormsModule
   ],
   templateUrl: './forum.component.html',
   styleUrl: './forum.component.css'
 })
 export class ForumComponent implements OnInit {
   type_user: number = 1; // here will be the type of the user bringing by event input, 1: team, 0: technician
+  id_user: string = "1";
   data: any[] = [];
   answers: any[] = [];
   showNewForm: boolean = false;
+  dataNewForum = {
+    title: "",
+    description: "",
+    user_owner: {
+      id: 0,
+      type_user: "",
+      name: "",
+      email: "",
+      password: "",
+    },
+    answers: []
+  };
 
-  constructor(private forumService: ForumService, private router: Router){
+  constructor(private forumService: ForumService, private router: Router,
+              private route: ActivatedRoute, private authService: AuthService){
   }
 
   ngOnInit() {
+    this.getTypeUser();
     this.getForumPosts();
   }
 
   getForumPosts() {
     this.forumService.getForumPosts().subscribe(
       result => {
-        for(let e in result) {
-          const post = new ForumCommentEntity(result[e].id,result[e].image, result[e].name, result[e].title, result[e].description, result[e].answers);
-          console.log(result[e].answers)
+        console.log(result)
+        result.forEach((forum: any)=> {
+          if(this.id_user == forum.id_user) {
+            this.data =forum.forum_posts;
+          }
+        })
 
-          this.data.push(post);
-        }
+
+
       },
       e => {
         console.log(e);
       }
     )
+  }
+
+  getTypeUser() {
+    this.route.params.subscribe(params => {
+      this.type_user = (params['type_user'] === "company")? 1 : 0;
+      this.id_user = params['id'];
+      console.log('id', this.id_user)
+    })
   }
 
   receiveAnswers(answers: any[]) {
@@ -70,4 +99,57 @@ export class ForumComponent implements OnInit {
     this.router.navigate(['forum','publish', id]);
   }
 
+  showNewForum(){
+    this.showNewForm = !this.showNewForm;
+  }
+
+  createNewForum() {
+    this.authService.getUsers().subscribe(
+      (result: any[]) => {
+        console.log('result', result)
+        this.dataNewForum.user_owner = result.find((user: any) => user.id === parseInt(this.id_user)) || {};
+
+        this.forumService.getForumPosts().subscribe(
+          (forums: any[]) => {
+            forums.forEach((user_forum: any) => {
+              console.log("userforum", user_forum);
+              if(user_forum.id_user == this.dataNewForum.user_owner.id) {
+                const body_new_post: any = {
+                  image: "https://raw.githubusercontent.com/AplicacionesWeb-WX54/si730-WX54-Grupo1-Repository/main/assets/members-profile/nekolas-profile.png",
+                  name: this.dataNewForum.user_owner.name,
+                  title: this.dataNewForum.title,
+                  description: this.dataNewForum.description,
+                  answers: this.dataNewForum.answers
+                }
+
+                console.log('user', this.dataNewForum.user_owner)
+
+                this.forumService.createForumPost(this.dataNewForum.user_owner.id, body_new_post).subscribe(
+                  result => {
+                    console.log('post created: ', result);
+                    this.getForumPosts();
+                    this.showNewForm = false;
+                  },
+                  e => {
+                    console.log(e);
+                  }
+                )
+              }
+
+            })
+          }
+        )
+
+
+
+
+
+
+
+      },
+      e => {
+        console.log(e);
+      }
+    )
+  }
 }
