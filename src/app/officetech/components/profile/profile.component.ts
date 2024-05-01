d5e7812a10d100cdfbe16b8e7f7a21e227e3d8ac
QuiqueVladimir <U202022365@upc.edu.pe>
2024-05-01 14:24:59 -0500

    fix(profile): added user data

--- src/app/officetech/components/profile/profile.component.ts
+++ src/app/officetech/components/profile/profile.component.ts
@@ -1,25 +1,58 @@
 import {Component, OnInit} from '@angular/core';
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {UserService} from "../../services/user/user.service.entity";
+import {UserEntity} from "../../models/user-entity";
+import {ActivatedRoute} from "@angular/router";
+import {NgIf} from "@angular/common";
+
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardActions,
+    MatCardContent,
+    MatCardTitle,
+    MatCardHeader,
+    MatCard,
+    NgIf
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
+
 export class ProfileComponent implements OnInit {
-  name: string = "";
-  email: string = "";
-  phone: string = "";
-  imgUrl: string = "";
 
-  constructor() { }
+  userData: UserEntity | undefined;
 
-  ngOnInit(): void {
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) { }
 
-    this.name = "Nombre del Usuario";
-    this.email = "usuario@example.com";
-    this.phone = "123456789";
-    this.imgUrl = "url_de_la_imagen";
+  ngOnInit() {
+    this.route.params.subscribe( params => {
+      console.log(params);
+      const id = +params['id'];
+      console.log('ID de usuario:', id);
+      if (!isNaN(id)) {
+         this.loadUser(id);
+      }else{
+        console.error('ID de usuario no válido:', id);
+      }
+    });
   }
-}
+    private loadUser(id: number) {
+      try{
+        this.userService.getUserData().subscribe(
+          (result) => {
+            this.userData = result.find(user => user.id === id);
+          }
+        )
+      } catch(error) {
+        console.error('Error al cargar datos de usuario:', error);
+      }
+    }
+  }
+
+
